e2a07c8796de4293cabfa9f80a8c4ae921c166ec
khangpm <khangpm88@gmail.com>
2020-12-26 22:01:40 +0700

    a

--- src/js/app.js
+++ src/js/app.js
@@ -20,7 +20,7 @@ import App from '../components/app.vue';
 // Init Framework7-Vue Plugin
 Framework7.use(Framework7Vue);
 
-console.log('ready')
+console.log('helllo')
 // Init App
 new Vue({
   el: '#app',
